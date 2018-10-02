b874182d1bec459d77f19ff4b239d99f141c6251
Laiwett <l.mendoza.liet@gmail.com>
2018-10-02 19:21:59 -0400

    cambios para trabajar localmente

--- app/config/db.php
+++ app/config/db.php
@@ -8,5 +8,5 @@
 	define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
 	define('DB_USER', 'root');//Usuario de tu base de datos
 	define('DB_PASS', '');//Contraseña del usuario de la base de datos
-	define('DB_NAME', 'marketbd_prod_final');//Nombre de la base de datos
+	define('DB_NAME', 'marketbbdd');//Nombre de la base de datos
 ?>
