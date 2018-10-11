7bf974b18f7cd50d73d825b1e7bc272c4ea9f0bc
laiwett <l.mendoza.liet@gmail.com>
2018-10-11 07:08:21 -0400

    bd con datos

--- bd/micromarketnew.sql
+++ bd/micromarketnew.sql
@@ -4,7 +4,7 @@
 -- SO del servidor:              Win32
 -- HeidiSQL Versión:             9.5.0.5196
 -- --------------------------------------------------------
-
+ 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
 /*!50503 SET NAMES utf8mb4 */;
@@ -60,6 +60,14 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   UNIQUE KEY `ci_UNIQUE` (`ci`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+-- Volcando datos para la tabla marketbbdd.cliente: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` (`id_cliente`, `ci`, `nombre`, `estado`, `fecha_registro`) VALUES
+	(2, '4038952', 'peppito', 1, '2018-08-11 21:00:42'),
+	(3, '1234567', 'juan', 1, '2018-08-11 21:27:02'),
+	(4, '123456', 'pepito', 1, '2018-08-11 22:38:44');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+
 -- La exportación de datos fue deseleccionada.
 -- Volcando estructura para tabla marketbbdd.compra_r
 CREATE TABLE IF NOT EXISTS `compra_r` (
@@ -78,6 +86,15 @@ CREATE TABLE IF NOT EXISTS `compra_r` (
   CONSTRAINT `FK_USER_DET` FOREIGN KEY (`id_usuario`) REFERENCES `usuario_login` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+-- Volcando datos para la tabla marketbbdd.compra_r: ~5 rows (aproximadamente)
+/*!40000 ALTER TABLE `compra_r` DISABLE KEYS */;
+INSERT INTO `compra_r` (`id_compra`, `nro_recibo`, `total`, `total_literal`, `fecha`, `id_cliente`, `id_usuario`, `estado`) VALUES
+	(1, '1', 73.75, 'SETENTA Y TRES con 75/100 Bs.', '2018-08-11 21:00:53', 2, 2, NULL),
+	(2, '2', 73.75, 'SETENTA Y TRES con 75/100 Bs.', '2018-08-11 21:04:00', 2, 2, NULL),
+	(3, '3', 1.35, 'UNO con 35/100 Bs.', '2018-08-11 22:03:14', 3, 2, NULL),
+	(4, '4', 15.7, 'QUINCE con 70 100 Bs.', '2018-08-11 22:20:54', 3, 2, NULL),
+	(5, '5', 32.15, 'TREINTA Y DOS con 15/100 Bs.', '2018-08-11 22:40:18', 4, 3, NULL);
+/*!40000 ALTER TABLE `compra_r` ENABLE KEYS */;
 -- La exportación de datos fue deseleccionada.
 -- Volcando estructura para procedimiento marketbbdd.detalleVentasPorRangoFechas
 DELIMITER //
@@ -414,6 +431,105 @@ CREATE TABLE IF NOT EXISTS `producto` (
   CONSTRAINT `fk_producto_seccion1` FOREIGN KEY (`idseccion`) REFERENCES `seccion` (`id_seccion`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=155 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+-- Volcando datos para la tabla marketbbdd.producto: ~93 rows (aproximadamente)
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+INSERT INTO `producto` (`id_prod`, `nro_plu`, `nombre`, `tipo`, `precio`, `cod_plu`, `estado`, `fecha_registro`, `fecha_actualizacion`, `id_limite`, `idseccion`) VALUES
+	(62, 1, 'filete', 2, 50, '2001', 1, '2018-08-09 14:32:32', '2018-08-09 14:32:32', NULL, 1),
+	(63, 2, 'pulpa', 2, 45, '2002', 1, '2018-08-09 14:33:18', '2018-08-09 14:33:18', NULL, 1),
+	(64, 3, 'cabeza de lomo', 2, 45, '2003', 1, '2018-08-09 14:33:51', '2018-08-09 14:33:51', NULL, 1),
+	(65, 4, 'peceto', 2, 42, '2004', 1, '2018-08-09 14:34:13', '2018-08-09 14:34:13', NULL, 1),
+	(66, 5, 'punta de s', 2, 45, '2005', 1, '2018-08-09 14:34:35', '2018-08-09 14:34:35', NULL, 1),
+	(67, 6, 'molida', 2, 38, '2006', 1, '2018-08-09 14:34:57', '2018-08-09 14:34:57', NULL, 1),
+	(68, 7, 'albondiga', 2, 26, '2007', 1, '2018-08-09 14:35:21', '2018-08-09 14:35:21', NULL, 1),
+	(69, 8, 'milaneza de carne', 2, 18, '2008', 1, '2018-08-09 14:35:46', '2018-08-09 14:35:46', NULL, 1),
+	(70, 9, 'lapimg', 2, 36, '2009', 1, '2018-08-09 14:36:25', '2018-08-09 14:36:25', NULL, 1),
+	(71, 10, 'chuleta', 2, 38, '2010', 1, '2018-08-09 14:36:53', '2018-08-09 14:36:53', NULL, 1),
+	(72, 11, 'witu', 2, 36, '2011', 1, '2018-08-09 14:37:17', '2018-08-09 14:37:17', NULL, 1),
+	(73, 12, 'churrasco', 2, 36, '2012', 1, '2018-08-09 14:38:04', '2018-08-09 14:38:04', NULL, 1),
+	(74, 13, 'cadera', 2, 36, '2013', 1, '2018-08-09 14:38:27', '2018-08-09 14:38:27', NULL, 1),
+	(75, 14, 'paleta', 2, 36, '2014', 1, '2018-08-09 14:39:09', '2018-08-09 14:39:09', NULL, 1),
+	(76, 15, 'pecho', 2, 30, '2015', 1, '2018-08-09 14:39:35', '2018-08-09 14:39:35', NULL, 1),
+	(77, 16, 'jiba', 2, 30, '2016', 1, '2018-08-09 14:40:03', '2018-08-09 14:40:03', NULL, 1),
+	(78, 17, 'aujilla', 2, 30, '2017', 1, '2018-08-09 14:40:28', '2018-08-09 14:40:28', NULL, 1),
+	(79, 18, 'tira/costilla', 2, 34, '2018', 1, '2018-08-09 14:41:02', '2018-08-09 14:41:02', NULL, 1),
+	(80, 19, 'ozubuco', 2, 28, '2019', 1, '2018-08-09 14:41:27', '2018-08-09 14:41:27', NULL, 1),
+	(81, 20, 'canastitas', 2, 18, '2020', 1, '2018-08-09 14:41:49', '2018-08-09 14:41:49', NULL, 1),
+	(82, 21, 'chorizo', 2, 36, '2021', 1, '2018-08-09 14:42:11', '2018-08-09 14:42:11', NULL, 1),
+	(83, 22, 'corazon', 2, 30, '2022', 1, '2018-08-09 14:42:39', '2018-08-09 14:42:39', NULL, 1),
+	(84, 23, 'higado', 2, 22, '2023', 1, '2018-08-09 14:42:55', '2018-08-09 14:42:55', NULL, 1),
+	(85, 24, 'panza cocida', 2, 32, '2024', 1, '2018-08-09 14:43:20', '2018-08-09 14:43:20', NULL, 1),
+	(86, 25, 'pollo', 2, 15, '2025', 1, '2018-08-09 14:43:52', '2018-08-09 14:43:52', NULL, 1),
+	(87, 26, 'pierna de pollo', 2, 15, '2026', 1, '2018-08-09 14:44:09', '2018-08-09 14:44:09', NULL, 1),
+	(88, 27, 'alas de pollo', 2, 15, '2027', 1, '2018-08-09 14:44:32', '2018-08-09 14:44:32', NULL, 1),
+	(89, 28, 'pecho de pollo', 2, 15, '2028', 1, '2018-08-09 14:44:56', '2018-08-09 14:44:56', NULL, 1),
+	(90, 29, 'molida de pollo', 2, 12, '2029', 1, '2018-08-09 14:45:32', '2018-08-09 14:45:32', NULL, 1),
+	(91, 30, 'milaneza de pollo', 2, 16, '2030', 1, '2018-08-09 14:45:51', '2018-08-09 14:45:51', NULL, 1),
+	(92, 31, 'pescado trucha', 2, 45, '2031', 1, '2018-08-09 14:46:16', '2018-08-09 14:46:16', NULL, 1),
+	(93, 32, 'pescado pacu', 2, 50, '2032', 1, '2018-08-09 14:46:48', '2018-08-09 14:46:48', NULL, 1),
+	(94, 33, 'pescado surubi', 2, 50, '2033', 1, '2018-08-09 14:47:16', '2018-08-09 14:47:16', NULL, 1),
+	(95, 34, 'huevo25', 1, 25, '1034', 1, '2018-08-09 14:47:42', '2018-08-09 14:47:42', NULL, 1),
+	(96, 35, 'huevo22', 1, 0.73, '1035', 1, '2018-08-09 14:48:04', '2018-08-09 14:48:04', NULL, 1),
+	(97, 36, 'zanahoria', 2, 4, '2036', 1, '2018-08-09 14:49:13', '2018-08-09 14:49:13', NULL, 2),
+	(98, 37, 'haba', 2, 8, '2037', 1, '2018-08-09 14:49:54', '2018-08-09 14:49:54', NULL, 2),
+	(99, 38, 'zapallo', 2, 6, '2038', 1, '2018-08-09 14:58:25', '2018-08-09 14:58:25', NULL, 2),
+	(100, 39, 'cebolla con cola', 2, 4.5, '2039', 1, '2018-08-09 14:58:46', '2018-08-09 14:58:46', NULL, 2),
+	(101, 40, 'cebolla/cabeza', 2, 6, '2040', 1, '2018-08-09 14:59:08', '2018-08-09 14:59:08', NULL, 2),
+	(102, 41, 'tomate', 2, 9, '2041', 1, '2018-08-09 14:59:45', '2018-08-09 14:59:45', NULL, 2),
+	(103, 42, 'achojcha', 2, 3, '2042', 1, '2018-08-09 15:00:44', '2018-08-09 15:00:44', NULL, 2),
+	(104, 43, 'remolacha', 2, 4, '2043', 1, '2018-08-09 15:01:24', '2018-08-09 15:01:24', NULL, 2),
+	(105, 44, 'vainita', 2, 9, '2044', 1, '2018-08-09 15:01:49', '2018-08-09 15:01:49', NULL, 2),
+	(106, 45, 'arveja', 2, 15, '2045', 1, '2018-08-09 15:02:35', '2018-08-09 15:02:35', NULL, 2),
+	(107, 46, 'yuca', 2, 8.5, '2046', 1, '2018-08-09 15:02:54', '2018-08-09 15:02:54', NULL, 2),
+	(108, 47, 'perejil', 2, 3, '2047', 1, '2018-08-09 15:03:16', '2018-08-09 15:03:16', NULL, 2),
+	(109, 48, 'camote', 2, 11.5, '2048', 1, '2018-08-09 15:03:41', '2018-08-09 15:03:41', NULL, 2),
+	(110, 49, 'papa grande', 2, 6.5, '2049', 1, '2018-08-09 15:04:03', '2018-08-09 15:04:03', NULL, 2),
+	(111, 50, 'papa pequeÃ±a', 2, 5, '2050', 1, '2018-08-09 15:04:28', '2018-08-09 15:04:28', NULL, 2),
+	(112, 51, 'papa holandesa', 2, 6, '2051', 1, '2018-08-09 15:04:56', '2018-08-09 15:04:56', NULL, 2),
+	(113, 52, 'papa liza', 2, 4, '2052', 1, '2018-08-09 15:05:10', '2018-08-09 15:05:10', NULL, 2),
+	(114, 53, 'papa morada', 2, 4.5, '2053', 1, '2018-08-09 15:05:30', '2018-08-09 15:05:30', NULL, 2),
+	(115, 54, 'espinaca', 2, 10, '2054', 1, '2018-08-09 15:07:15', '2018-08-09 15:07:15', NULL, 2),
+	(116, 55, 'brocoli', 2, 10, '2055', 1, '2018-08-09 15:07:33', '2018-08-09 15:07:33', NULL, 2),
+	(117, 56, 'apio', 2, 3, '2056', 1, '2018-08-09 15:08:09', '2018-08-09 15:08:09', NULL, 2),
+	(118, 57, 'acelga', 2, 4, '2057', 1, '2018-08-09 15:08:47', '2018-08-09 15:08:47', NULL, 2),
+	(119, 58, 'repollo', 2, 4, '2058', 1, '2018-08-09 15:09:06', '2018-08-09 15:09:06', NULL, 2),
+	(120, 59, 'coliflor', 2, 3, '2059', 1, '2018-08-09 15:09:29', '2018-08-09 15:09:29', NULL, 2),
+	(121, 60, 'berro', 2, 5, '2060', 1, '2018-08-09 15:09:54', '2018-08-09 15:09:54', NULL, 2),
+	(122, 61, 'pepino', 2, 2, '2061', 1, '2018-08-09 15:10:10', '2018-08-09 15:10:10', NULL, 2),
+	(123, 62, 'berenjena', 2, 5, '2062', 1, '2018-08-09 15:10:24', '2018-08-09 15:10:24', NULL, 2),
+	(124, 63, 'pimenton', 2, 1.5, '2063', 1, '2018-08-09 15:10:42', '2018-08-09 15:10:42', NULL, 2),
+	(125, 64, 'platano de freir', 1, 1.5, '1064', 1, '2018-08-09 15:11:00', '2018-08-09 15:11:00', NULL, 2),
+	(126, 65, 'durazno', 2, 1, '2065', 1, '2018-08-09 15:19:44', '2018-08-09 15:19:44', NULL, 3),
+	(127, 66, 'uva', 2, 1, '2066', 1, '2018-08-09 15:20:04', '2018-08-09 15:20:04', NULL, 3),
+	(128, 67, 'frutilla', 2, 1, '2067', 1, '2018-08-09 15:20:18', '2018-08-09 15:20:18', NULL, 3),
+	(129, 68, 'achachairu', 2, 1, '2068', 1, '2018-08-09 15:20:50', '2018-08-09 15:20:50', NULL, 3),
+	(130, 69, 'asai', 2, 1, '2069', 1, '2018-08-09 15:21:22', '2018-08-09 15:21:22', NULL, 3),
+	(131, 70, 'sandia', 2, 1, '2070', 1, '2018-08-09 15:22:01', '2018-08-09 15:22:01', NULL, 3),
+	(132, 71, 'chirimoya', 1, 1, '1071', 1, '2018-08-09 15:22:22', '2018-08-09 15:22:22', NULL, 3),
+	(133, 72, 'mango', 1, 1, '1072', 1, '2018-08-09 15:22:45', '2018-08-09 15:22:45', NULL, 3),
+	(134, 73, 'papaya', 1, 1, '1073', 1, '2018-08-09 15:23:00', '2018-08-09 15:23:00', NULL, 3),
+	(135, 74, 'pera', 1, 1, '1074', 1, '2018-08-09 15:23:17', '2018-08-09 15:23:17', NULL, 3),
+	(136, 75, 'tuna', 1, 1, '1075', 1, '2018-08-09 15:23:34', '2018-08-09 15:23:34', NULL, 3),
+	(137, 76, 'pomelo', 1, 1, '1076', 1, '2018-08-09 15:23:46', '2018-08-09 15:23:46', NULL, 3),
+	(138, 77, 'granada', 1, 1, '1077', 1, '2018-08-09 15:24:01', '2018-08-09 15:24:01', NULL, 3),
+	(139, 78, 'guayaba', 1, 1, '1078', 1, '2018-08-09 15:24:14', '2018-08-09 15:24:14', NULL, 3),
+	(140, 79, 'lima', 1, 1, '1079', 1, '2018-08-09 15:24:26', '2018-08-09 15:24:26', NULL, 3),
+	(141, 80, 'ciruelo', 1, 1, '1080', 1, '2018-08-09 15:24:40', '2018-08-09 15:24:40', NULL, 3),
+	(142, 81, 'palta', 1, 1, '1081', 1, '2018-08-09 15:24:54', '2018-08-09 15:24:54', NULL, 3),
+	(143, 82, 'menbrillo', 1, 1, '1082', 1, '2018-08-09 15:25:07', '2018-08-09 15:25:07', NULL, 3),
+	(144, 83, 'maracuya', 1, 1, '1083', 1, '2018-08-09 15:25:41', '2018-08-09 15:25:41', NULL, 3),
+	(145, 84, 'mandarina', 1, 1, '1084', 1, '2018-08-09 15:25:59', '2018-08-09 15:25:59', NULL, 3),
+	(146, 85, 'melon', 1, 1, '1085', 1, '2018-08-09 15:26:13', '2018-08-09 15:26:13', NULL, 3),
+	(147, 86, 'higo', 1, 1, '1086', 1, '2018-08-09 15:26:25', '2018-08-09 15:26:25', NULL, 3),
+	(148, 87, 'platano', 1, 1, '1087', 1, '2018-08-09 15:26:38', '2018-08-09 15:26:38', NULL, 3),
+	(149, 88, 'manzana', 1, 1, '1088', 1, '2018-08-09 15:26:58', '2018-08-09 15:26:58', NULL, 3),
+	(150, 89, 'piÃ±a', 1, 1, '1089', 1, '2018-08-09 15:27:12', '2018-08-09 15:27:12', NULL, 3),
+	(151, 90, 'copoazu', 1, 1, '1090', 1, '2018-08-09 15:27:25', '2018-08-09 15:27:25', NULL, 3),
+	(152, 91, 'durazno (oregon)', 2, 1, '2091', 1, '2018-08-09 15:27:39', '2018-08-09 15:27:39', NULL, 3),
+	(153, 92, 'limon', 1, 1, '1092', 1, '2018-08-09 15:27:52', '2018-08-09 15:27:52', NULL, 3),
+	(154, 93, 'naranja', 1, 5, '1093', 1, '2018-08-09 15:28:06', '2018-08-09 15:28:06', NULL, 3);
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+
+
 -- La exportación de datos fue deseleccionada.
 -- Volcando estructura para procedimiento marketbbdd.producto_categoria
 DELIMITER //
@@ -439,6 +555,27 @@ CREATE TABLE IF NOT EXISTS `producto_etiquetado` (
   CONSTRAINT `FK_COMPRA_DET` FOREIGN KEY (`id_compra`) REFERENCES `compra_r` (`id_compra`),
   CONSTRAINT `FK_PROD_DET` FOREIGN KEY (`id_prod`) REFERENCES `producto` (`id_prod`)
 ) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- Volcando datos para la tabla marketbbdd.producto_etiquetado: ~16 rows (aproximadamente)
+/*!40000 ALTER TABLE `producto_etiquetado` DISABLE KEYS */;
+INSERT INTO `producto_etiquetado` (`id_etiqueta`, `cod_barras`, `preciototal`, `peso_cantidad`, `estado`, `id_prod`, `id_compra`) VALUES
+	(16, 2020010029516, 14.75, 0.295, 1, 62, 1),
+	(17, 2020010029516, 14.75, 0.295, 1, 62, 1),
+	(18, 2020010029516, 14.75, 0.295, 1, 62, 1),
+	(19, 2020010029516, 14.75, 0.295, 1, 62, 1),
+	(20, 2020010029516, 14.75, 0.295, 1, 62, 1),
+	(21, 2020010029516, 14.75, 0.295, 1, 62, 2),
+	(22, 2020010029516, 14.75, 0.295, 1, 62, 2),
+	(23, 2020010029516, 14.75, 0.295, 1, 62, 2),
+	(24, 2020010029516, 14.75, 0.295, 1, 62, 2),
+	(25, 2020010029516, 14.75, 0.295, 1, 62, 2),
+	(26, 2020440015035, 1.35, 0.15, 1, 105, 3),
+	(27, 2110890001037, 10, 10, 1, 150, 4),
+	(28, 2020060015033, 5.7, 0.15, 1, 67, 4),
+	(29, 1034, 20.75, 25, 1, 95, 5),
+	(30, 2020060015033, 5.7, 0.15, 1, 67, 5),
+	(31, 2020060015033, 5.7, 0.15, 1, 67, 5);
+/*!40000 ALTER TABLE `producto_etiquetado` ENABLE KEYS */;
 
 -- La exportación de datos fue deseleccionada.
 -- Volcando estructura para procedimiento marketbbdd.Recibo_cliente
@@ -475,6 +612,14 @@ CREATE TABLE IF NOT EXISTS `seccion` (
   UNIQUE KEY `nombre_UNIQUE` (`nombre`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
+-- Volcando datos para la tabla marketbbdd.seccion: ~3 rows (aproximadamente)
+/*!40000 ALTER TABLE `seccion` DISABLE KEYS */;
+INSERT INTO `seccion` (`id_seccion`, `nombre`, `limite`, `estado`, `fecha_registro`, `fecha_actualizacion`, `id_categoria`) VALUES
+	(1, 'CARNE Y DERIVADOS', 0, 1, '2018-08-02 22:34:31', '2018-08-02 16:34:31', NULL),
+	(2, 'VERDURAS RAICES Y TUBERCULOS', 0, 1, '2018-08-02 22:35:08', '2018-08-02 16:35:08', NULL),
+	(3, 'FRUTAS', 0, 1, '2018-08-02 22:35:40', '2018-08-02 16:35:40', NULL);
+/*!40000 ALTER TABLE `seccion` ENABLE KEYS */;
+
 -- La exportación de datos fue deseleccionada.
 -- Volcando estructura para procedimiento marketbbdd.usuario_estado1
 DELIMITER //
@@ -497,6 +642,16 @@ CREATE TABLE IF NOT EXISTS `usuario_login` (
   PRIMARY KEY (`id_usuario`),
   UNIQUE KEY `usuario_UNIQUE` (`usuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- Volcando datos para la tabla marketbbdd.usuario_login: ~4 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuario_login` DISABLE KEYS */;
+INSERT INTO `usuario_login` (`id_usuario`, `nombre`, `usuario`, `contrasenia`, `estado`, `tipo`, `fecha_registro`, `fecha_actualizacion`) VALUES
+	(1, 'Haki Ari', 'hakiari.com', '$2y$10$kFXCe9R5NugF6b4PzarJqulRGCqbIk4E22F3Ouw/RWp2YLH/g41oe', 1, 0, '2018-02-01 02:26:10', '2018-02-07 10:11:35'),
+	(2, 'josue daniel gutierrez quispe', 'josuedg', '$2y$10$haU7WOd1e8lFE/pG0HhJcumVGv4QcqV1Yp2CT3U9/l27LYTsoEr.u', 1, 1, '2018-08-11 18:48:59', '2018-08-11 14:48:59'),
+	(3, 'abad alvarez claros', 'aalvarez', '$2y$10$c2iKNzURy0P5AVfg0KWrj.7gaI/Fu.9hRZIL7zNrsSTjixmX8/dPu', 1, 1, '2018-08-11 22:27:43', '2018-08-11 18:27:43'),
+	(4, 'delia olguin orihuela', 'dolguin', '$2y$10$I8mvCGNI.2wjeI/hEb7rhOSCvgmFAY7bk1kouZlE4jFlEth0mjI9.', 1, 2, '2018-08-11 22:34:45', '2018-08-11 18:34:45'),
+	(5, 'Luis Miguel Mendoza Ticona', 'lmendoza', '$2y$10$cX8BWe4o.TWTu2uoHt.16exSpaDaUfIckgVVVe5KlPhcqb/pieZGK', 1, 1, '2018-10-06 19:23:09', '2018-10-06 13:23:09');
+/*!40000 ALTER TABLE `usuario_login` ENABLE KEYS */;
 
 -- La exportación de datos fue deseleccionada.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
