f8427d76fbde3226ea30c674280aeaf164224549
sergui <juansergio1981@gmail.com>
2018-11-22 18:10:49 -0400

    modelo de seccion acabado

--- app/models/seccion/registro_model.php
+++ app/models/seccion/registro_model.php
@@ -5,10 +5,17 @@
 
 	//echo "<pre>";print_r ($_REQUEST);echo "</pre>";
 	$nombre = trim($_POST["nombre"]);
-	$limite = 0;
+	$lim1=trim($_POST["limite1"]);
+	$lim2=trim($_POST["limite2"]);
+	$lim3=trim($_POST["limite3"]);
+	
+	//$valorTicket =$lim1.$lim2.$lim3;
+	$valorTicket =$lim1." ".$lim2." ".$lim3;
 	$fechaRegistro = date('Y-m-d H:i:s');
 
-	$sql = "INSERT INTO seccion(nombre, limite, fecha_registro) VALUES('{$nombre}', {$limite},'{$fechaRegistro}')";
+
+
+	$sql = "INSERT INTO seccion(nombre,valor_ticket, fecha_registro) VALUES('{$nombre}', '{$valorTicket}','{$fechaRegistro}')";
 
 	if (!$con->query($sql)) {
 		echo "Falló la insercion: (" . $con->errno . ") " . $con->error;
