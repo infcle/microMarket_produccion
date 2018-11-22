3c12514592871601b65aa8f24f49744bdbda5775
sergui <juansergio1981@gmail.com>
2018-11-22 10:39:23 -0400

    vista de seccion con limites de ticket
    sugerencia sino me avisan

--- public/views/seccion/modal_registrar.php
+++ public/views/seccion/modal_registrar.php
@@ -7,11 +7,33 @@
             </div>
             <div class="modal-body">
                 <form role="form" id="frmRegistrar" name="frmRegistrar">
-                    <div class="form-group>
+
+                    <div class="form-group">
                         <label for="Nombre">Nombre de Seccion</label>
-                        <input class=" form-control" id="nombre" name="nombre" type="text" />
+                        <input class="form-control" id="nombre" name="nombre" type="text" />
                     </div>
-                    <div class=" modal-footer">
+                      <span>Limites por ticket</span> <br><br>
+
+                            <div class="form-group row">
+
+                              <div class="col-xs-2">
+                                 <label>ticket 1</label>
+                                 <input class="form-control btn btn-info" id="lim1" name="lim1" type="text">
+                              </div>
+
+                             <div class="col-xs-2">
+                                 <label>ticket 2</label>
+                                 <input class="form-control btn btn-info" id="lim2" name="lim2" type="text">
+                             </div>
+
+                              <div class="col-xs-2">
+                                 <label>ticket 3</label>
+                                 <input class="form-control btn btn-info" id="lim3" name="lim3" type="text">
+                             </div>
+                            </div>
+
+                    <br><br><br><br><br>
+                    <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
                         <button type="submit" class="btn btn-primary" id="btnRegistrar" >Crear</button>
                     </div>
