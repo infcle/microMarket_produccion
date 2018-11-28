d94a0e681b23318f2edda7e6391f7f07a7c0737a
sergui <juansergio1981@gmail.com>
2018-11-28 20:37:09 +0000

    actualizacion seccion y productos de la base de datod

--- bd/micromarketbd.sql
+++ bd/micromarketbd.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Enterprise v12.09 (64 bit)
-MySQL - 10.1.9-MariaDB : Database - marketbbdd
+SQLyog Community Edition- MySQL GUI v8.05 
+MySQL - 5.5.5-10.1.34-MariaDB : Database - marketbbdd
 *********************************************************************
 */
 
@@ -9,10 +9,9 @@ MySQL - 10.1.9-MariaDB : Database - marketbbdd
 
 /*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`marketbbdd` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `marketbbdd`;
@@ -81,11 +80,11 @@ CREATE TABLE `producto` (
   UNIQUE KEY `cod_plu_UNIQUE` (`cod_plu`),
   KEY `fk_producto_seccion1_idx` (`idseccion`),
   CONSTRAINT `fk_producto_seccion1` FOREIGN KEY (`idseccion`) REFERENCES `seccion` (`id_seccion`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
 
 /*Data for the table `producto` */
 
-insert  into `producto`(`id_prod`,`nro_plu`,`nombre`,`tipo`,`precio`,`cod_plu`,`estado`,`fecha_registro`,`fecha_actualizacion`,`id_limite`,`idseccion`) values (1,1,'filete',2,50,'2001',1,'2018-11-28 11:49:31','2018-11-28 11:49:31',NULL,1),(2,2,'pulpa',2,48,'2002',1,'2018-11-28 11:51:39','2018-11-28 11:51:39',NULL,1);
+insert  into `producto`(`id_prod`,`nro_plu`,`nombre`,`tipo`,`precio`,`cod_plu`,`estado`,`fecha_registro`,`fecha_actualizacion`,`id_limite`,`idseccion`) values (1,1,'filete',2,50,'2001',1,'2018-11-28 11:49:31','2018-11-28 11:49:31',NULL,1),(2,2,'pulpa',2,48,'2002',1,'2018-11-28 11:51:39','2018-11-28 11:51:39',NULL,1),(3,3,'cabeza de lomo',2,45,'2003',1,'2018-11-28 18:42:13','2018-11-28 18:42:13',NULL,1),(4,4,'peceto',2,42,'2004',1,'2018-11-28 18:43:06','2018-11-28 18:43:06',NULL,1),(5,5,'punta de s',2,45,'2005',1,'2018-11-28 18:43:31','2018-11-28 18:43:31',NULL,1),(6,6,'molida',2,38,'2006',1,'2018-11-28 18:44:39','2018-11-28 18:44:39',NULL,1),(7,7,'albondiga',2,26,'2007',1,'2018-11-28 18:45:05','2018-11-28 18:45:05',NULL,1),(8,8,'milaneza de carne',1,28,'1008',1,'2018-11-28 18:45:51','2018-11-28 18:45:51',NULL,1),(9,9,'lapimg',2,36,'2009',1,'2018-11-28 18:46:16','2018-11-28 18:46:16',NULL,1),(10,10,'chuleta',2,43,'2010',1,'2018-11-28 18:47:11','2018-11-28 18:47:11',NULL,1),(11,11,'witu',2,36,'2011',1,'2018-11-28 19:06:40','2018-11-28 19:06:40',NULL,1),(12,12,'churrasco',2,36,'2012',1,'2018-11-28 19:07:10','2018-11-28 19:07:10',NULL,1),(13,13,'cadera',2,36,'2013',1,'2018-11-28 19:07:38','2018-11-28 19:07:38',NULL,1),(14,14,'paleta',2,36,'2014',1,'2018-11-28 19:08:16','2018-11-28 19:08:16',NULL,1),(15,15,'pecho',2,30,'2015',1,'2018-11-28 19:08:45','2018-11-28 19:08:45',NULL,1),(16,16,'jiba',2,30,'2016',1,'2018-11-28 19:09:19','2018-11-28 19:09:19',NULL,1),(17,17,'aujilla',2,34,'2017',1,'2018-11-28 19:09:54','2018-11-28 19:09:54',NULL,1),(18,18,'tira/costilla',2,34,'2018',1,'2018-11-28 19:10:22','2018-11-28 19:10:22',NULL,1),(19,19,'ozubuco',2,28,'2019',1,'2018-11-28 19:10:44','2018-11-28 19:10:44',NULL,1),(20,20,'canastitas',2,18,'2020',1,'2018-11-28 19:11:26','2018-11-28 19:11:26',NULL,1),(21,21,'chorizo',2,36,'2021',1,'2018-11-28 19:12:09','2018-11-28 19:12:09',NULL,1),(22,22,'corazon',2,30,'2022',1,'2018-11-28 19:12:56','2018-11-28 19:12:56',NULL,1),(23,23,'higado',2,22,'2023',1,'2018-11-28 19:14:12','2018-11-28 19:14:12',NULL,1),(24,24,'panza cocida',2,32,'2024',1,'2018-11-28 19:14:43','2018-11-28 19:14:43',NULL,1),(25,25,'hamburguesa de res',2,29,'2025',1,'2018-11-28 19:15:47','2018-11-28 19:15:47',NULL,1),(26,26,'silpancho de res',1,34,'1026',1,'2018-11-28 19:16:18','2018-11-28 19:16:18',NULL,1),(27,27,'huevo25',1,0.85,'1027',1,'2018-11-28 19:17:47','2018-11-28 19:17:47',NULL,1),(28,28,'huevo22',1,0.75,'1028',1,'2018-11-28 19:18:13','2018-11-28 19:18:13',NULL,1),(29,29,'pollo',2,16.5,'2029',1,'2018-11-28 19:36:09','2018-11-28 19:36:09',NULL,2),(30,30,'pierna de pollo',2,15,'2030',1,'2018-11-28 19:36:42','2018-11-28 19:36:42',NULL,2),(31,31,'alas de pollo',2,15,'2031',1,'2018-11-28 19:37:48','2018-11-28 19:37:48',NULL,2),(32,32,'pecho de pollo',2,15,'2032',1,'2018-11-28 19:38:22','2018-11-28 19:38:22',NULL,2),(33,33,'pescado trucha',2,45,'2033',1,'2018-11-28 19:39:05','2018-11-28 19:39:05',NULL,2),(34,34,'pescado pacu',2,50,'2034',1,'2018-11-28 19:39:35','2018-11-28 19:39:35',NULL,2),(35,35,'pescado surubi',2,50,'2035',1,'2018-11-28 19:40:00','2018-11-28 19:40:00',NULL,2),(36,36,'zanahoria',2,5,'2036',1,'2018-11-28 19:42:30','2018-11-28 19:42:30',NULL,3),(37,37,'haba',2,6,'2037',1,'2018-11-28 19:43:16','2018-11-28 19:43:16',NULL,3),(38,38,'zapallo',2,5.7,'2038',1,'2018-11-28 19:43:48','2018-11-28 19:43:48',NULL,3),(39,39,'cebolla con cola',2,4.5,'2039',1,'2018-11-28 19:45:05','2018-11-28 19:45:05',NULL,3),(40,40,'cebolla/cabeza',2,4.2,'2040',1,'2018-11-28 19:45:28','2018-11-28 19:45:28',NULL,3),(41,41,'tomate',2,8.3,'2041',1,'2018-11-28 19:46:05','2018-11-28 19:46:05',NULL,3),(42,42,'achojcha',2,12,'2042',1,'2018-11-28 19:47:52','2018-11-28 19:47:52',NULL,3),(43,43,'remolacha',2,3,'2043',1,'2018-11-28 19:48:30','2018-11-28 19:48:30',NULL,3),(44,44,'vainita',2,7,'2044',1,'2018-11-28 19:49:01','2018-11-28 19:49:01',NULL,3),(45,45,'arveja',2,8,'2045',1,'2018-11-28 19:57:28','2018-11-28 19:57:28',NULL,3),(46,46,'perejil',2,12,'2046',1,'2018-11-28 19:57:54','2018-11-28 19:57:54',NULL,3),(47,47,'espinaca',2,6,'2047',1,'2018-11-28 19:58:26','2018-11-28 19:58:26',NULL,3),(48,48,'brocoli',1,5,'1048',1,'2018-11-28 19:58:58','2018-11-28 19:58:58',NULL,3),(49,49,'apio',2,9,'2049',1,'2018-11-28 19:59:25','2018-11-28 19:59:25',NULL,3),(50,50,'acelga',2,8,'2050',1,'2018-11-28 19:59:55','2018-11-28 19:59:55',NULL,3),(51,51,'repollo',1,5,'1051',1,'2018-11-28 20:00:23','2018-11-28 20:00:23',NULL,3),(52,52,'coliflor',2,3,'2052',1,'2018-11-28 20:01:22','2018-11-28 20:01:22',NULL,3),(53,53,'berro',2,5,'2053',1,'2018-11-28 20:01:53','2018-11-28 20:01:53',NULL,3),(54,54,'pepino',1,5,'1054',1,'2018-11-28 20:02:25','2018-11-28 20:02:25',NULL,3),(55,55,'berenjena',2,5,'2055',1,'2018-11-28 20:02:48','2018-11-28 20:02:48',NULL,3),(56,56,'pimenton',2,7,'2056',1,'2018-11-28 20:03:14','2018-11-28 20:03:14',NULL,3),(57,57,'platano de freir',1,1,'1057',1,'2018-11-28 20:03:49','2018-11-28 20:03:49',NULL,3),(58,58,'yuca',2,4.3,'2058',1,'2018-11-28 20:07:28','2018-11-28 20:07:28',NULL,4),(59,59,'camote',2,6.5,'2059',1,'2018-11-28 20:07:53','2018-11-28 20:07:53',NULL,4),(60,60,'papa grande',2,3.8,'2060',1,'2018-11-28 20:08:33','2018-11-28 20:08:33',NULL,4),(61,61,'papa pequeÃ±a',2,5,'2061',1,'2018-11-28 20:11:03','2018-11-28 20:11:03',NULL,4),(62,62,'papa holandesa',2,6,'2062',1,'2018-11-28 20:12:03','2018-11-28 20:12:03',NULL,4),(63,63,'papa liza',2,4,'2063',1,'2018-11-28 20:12:25','2018-11-28 20:12:25',NULL,4),(64,64,'papa morada',2,4.5,'2064',1,'2018-11-28 20:12:53','2018-11-28 20:12:53',NULL,4),(65,65,'durazno',2,9,'2065',1,'2018-11-28 20:15:24','2018-11-28 20:15:24',NULL,5),(66,66,'uva',2,25,'2066',1,'2018-11-28 20:16:06','2018-11-28 20:16:06',NULL,5),(67,67,'frutilla',2,1,'2067',1,'2018-11-28 20:16:41','2018-11-28 20:16:41',NULL,5),(68,68,'achachairu',2,1,'2068',1,'2018-11-28 20:17:22','2018-11-28 20:17:22',NULL,5),(69,69,'asai',2,1,'2069',1,'2018-11-28 20:17:45','2018-11-28 20:17:45',NULL,5),(70,70,'sandia',1,10,'1070',1,'2018-11-28 20:19:01','2018-11-28 20:19:01',NULL,5),(71,71,'chirimoya',1,1,'1071',1,'2018-11-28 20:19:24','2018-11-28 20:19:24',NULL,5),(72,72,'mango',1,1.5,'1072',1,'2018-11-28 20:19:55','2018-11-28 20:19:55',NULL,5),(73,73,'papaya',1,10,'1073',1,'2018-11-28 20:20:25','2018-11-28 20:20:25',NULL,5),(74,74,'pera',1,1,'1074',1,'2018-11-28 20:20:52','2018-11-28 20:20:52',NULL,5),(75,75,'tuna',1,1,'1075',1,'2018-11-28 20:21:22','2018-11-28 20:21:22',NULL,5),(76,76,'pomelo',1,1,'1076',1,'2018-11-28 20:21:41','2018-11-28 20:21:41',NULL,5),(77,77,'granada',1,1,'1077',1,'2018-11-28 20:22:02','2018-11-28 20:22:02',NULL,5),(78,78,'guayaba',1,1,'1078',1,'2018-11-28 20:22:28','2018-11-28 20:22:28',NULL,5),(79,79,'lima',1,1,'1079',1,'2018-11-28 20:22:49','2018-11-28 20:22:49',NULL,5),(80,80,'ciruelo',1,1,'1080',1,'2018-11-28 20:23:04','2018-11-28 20:23:04',NULL,5),(81,81,'palta',1,3.5,'1081',1,'2018-11-28 20:23:28','2018-11-28 20:23:28',NULL,5),(82,82,'menbrillo',1,1,'1082',1,'2018-11-28 20:23:52','2018-11-28 20:23:52',NULL,5),(83,83,'maracuya',1,1,'1083',1,'2018-11-28 20:24:10','2018-11-28 20:24:10',NULL,5),(84,84,'mandarina',1,1,'1084',1,'2018-11-28 20:24:32','2018-11-28 20:24:32',NULL,5),(85,85,'melon',1,1,'1085',1,'2018-11-28 20:25:03','2018-11-28 20:25:03',NULL,5),(86,86,'higo',1,1,'1086',1,'2018-11-28 20:25:24','2018-11-28 20:25:24',NULL,5),(87,87,'platano',1,0.6,'1087',1,'2018-11-28 20:25:40','2018-11-28 20:25:40',NULL,5),(88,88,'manzana',1,1.5,'1088',1,'2018-11-28 20:26:23','2018-11-28 20:26:23',NULL,5),(89,89,'piÃ±a',1,7,'1089',1,'2018-11-28 20:26:53','2018-11-28 20:26:53',NULL,5),(90,90,'copoazu',1,1,'1090',1,'2018-11-28 20:27:17','2018-11-28 20:27:17',NULL,5),(91,91,'durazno (oregon)',2,1,'2091',1,'2018-11-28 20:27:55','2018-11-28 20:27:55',NULL,5),(92,92,'limon',1,0.6,'1092',1,'2018-11-28 20:28:19','2018-11-28 20:28:19',NULL,5),(93,93,'naranja',1,0.6,'1093',1,'2018-11-28 20:28:47','2018-11-28 20:28:47',NULL,5),(94,94,'mango pequeÃ±o',1,0.6,'1094',1,'2018-11-28 20:29:22','2018-11-28 20:29:22',NULL,5);
 
 /*Table structure for table `producto_etiquetado` */
 
@@ -467,6 +466,60 @@ UPDATE producto set nombre=nombre_p, precio=precio_p, tipo=tipo_p, cod_plu=plu, 
 end */$$
 DELIMITER ;
 
+/* Procedure structure for procedure `obtener_id_producto` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `obtener_id_producto` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `obtener_id_producto`( in nro_plu_F int)
+BEGIN
+       select id_prod from producto where nro_plu=nro_plu_F;
+    END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `producto_categoria` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `producto_categoria` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `producto_categoria`(
+in cat bigint)
+BEGIN
+select * from producto where id_cat=cat;
+END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `Recibo_cliente` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `Recibo_cliente` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `Recibo_cliente`(in id_r int)
+BEGIN
+select r.nro_recibo, r.fecha , c.nombre , c.ci, r.total, r.total_literal, r.fecha from compra_r r, cliente c where c.id_cliente=r.id_cliente and r.id_compra=id_r;
+END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `reporte_dia` */
+
+/*!50003 DROP PROCEDURE IF EXISTS  `reporte_dia` */;
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `reporte_dia`()
+    COMMENT 'crea el reporte de ventas por dia por caja o usuario que atendia'
+BEGIN
+select r.nro_recibo, r.fecha , c.nombre , c.ci, r.total,p.nombre, p.precio, pe.peso_cantidad, pe.preciototal
+  from cliente c,producto p, producto_etiquetado pe, compra_r r 
+    where c.id_cliente=r.id_cliente 
+      and c.ci='6846341' and r.fecha='2018-03-05 15:25:14'  
+            and p.id_prod=pe.id_prod ;
+END */$$
+DELIMITER ;
+
 /* Procedure structure for procedure `Numeros_a_Letras` */
 
 /*!50003 DROP PROCEDURE IF EXISTS  `Numeros_a_Letras` */;
@@ -582,60 +635,6 @@ SELECT RTRIM(lcRetorno) ;
 END */$$
 DELIMITER ;
 
-/* Procedure structure for procedure `obtener_id_producto` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `obtener_id_producto` */;
-
-DELIMITER $$
-
-/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `obtener_id_producto`( in nro_plu_F int)
-BEGIN
-       select id_prod from producto where nro_plu=nro_plu_F;
-    END */$$
-DELIMITER ;
-
-/* Procedure structure for procedure `producto_categoria` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `producto_categoria` */;
-
-DELIMITER $$
-
-/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `producto_categoria`(
-in cat bigint)
-BEGIN
-select * from producto where id_cat=cat;
-END */$$
-DELIMITER ;
-
-/* Procedure structure for procedure `Recibo_cliente` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `Recibo_cliente` */;
-
-DELIMITER $$
-
-/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `Recibo_cliente`(in id_r int)
-BEGIN
-select r.nro_recibo, r.fecha , c.nombre , c.ci, r.total, r.total_literal, r.fecha from compra_r r, cliente c where c.id_cliente=r.id_cliente and r.id_compra=id_r;
-END */$$
-DELIMITER ;
-
-/* Procedure structure for procedure `reporte_dia` */
-
-/*!50003 DROP PROCEDURE IF EXISTS  `reporte_dia` */;
-
-DELIMITER $$
-
-/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `reporte_dia`()
-    COMMENT 'crea el reporte de ventas por dia por caja o usuario que atendia'
-BEGIN
-select r.nro_recibo, r.fecha , c.nombre , c.ci, r.total,p.nombre, p.precio, pe.peso_cantidad, pe.preciototal
-  from cliente c,producto p, producto_etiquetado pe, compra_r r 
-    where c.id_cliente=r.id_cliente 
-      and c.ci='6846341' and r.fecha='2018-03-05 15:25:14'  
-            and p.id_prod=pe.id_prod ;
-END */$$
-DELIMITER ;
-
 /* Procedure structure for procedure `usuario_estado1` */
 
 /*!50003 DROP PROCEDURE IF EXISTS  `usuario_estado1` */;
@@ -650,5 +649,3 @@ DELIMITER ;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
